0c49dd61891487d71fa69a9b94c3e0d53e236c02
lxy <1755223527@qq.com>
2023-11-24 11:42:25 +0800

    第一次

---
+++ staticData/api.js
@@ -0,0 +1,11 @@
+var $onlineApi = {
+    'home_page': './staticData/json/home.json',
+
+    "lthx": './staticData/json/lthx/index.json',
+    "query_hx_list_gb": "./staticData/json/lthx/gb.json",
+    "query_hx_list_bz": "./staticData/json/lthx/bz.json",
+    "query_hx_list_gw" : "./staticData/json/lthx/gw.json",
+
+    "gbhx": "./staticData/json/gbhx/index.json",
+    "gwhx": "./staticData/json/gwhx/index.json"
+}
